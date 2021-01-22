4f24c8323d0dd6fb8abd58730e2db318e0777e09
jiangyunan33 <615921173@qq.com>
2021-01-22 16:05:55 +0800

    路由和样式引入

--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TableMain from '@/components/TableMain'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/tablemain',
+      name: 'TableMain',
+      component: TableMain
     }
   ]
 })
