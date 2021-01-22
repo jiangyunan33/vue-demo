b4f7858fafc1e1f6d05d80ea300e22d1ebf8f76b
jiangyunan33 <615921173@qq.com>
2021-01-22 17:05:46 +0800

    格式化

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -3,13 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//引入IVIEW组件
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-Vue.use(iView);
-//引入vue-easytable
+// 引入IVIEW组件
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+// 引入vue-easytable
 import 'vue-easytable/libs/themes-base/index.css'
-import {VTable,VPagination} from 'vue-easytable'
+import {VTable, VPagination} from 'vue-easytable'
+Vue.use(iView)
 Vue.component(VTable.name, VTable)
 Vue.component(VPagination.name, VPagination)
 
